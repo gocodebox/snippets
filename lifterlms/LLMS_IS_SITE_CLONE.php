d25d1fbbaebe4109bb5c78c61a65d1e1508bb72a
Thomas Patrick Levy <thomasplevy@gmail.com>
2021-05-17 12:14:50 -0700

    add clone definition

---
+++ lifterlms/LLMS_IS_SITE_CLONE.php
@@ -0,0 +1,12 @@
+<?php // Don't copy this line!
+/**
+ * current-user-within-course.php
+ *
+ * @since 2021-05-17
+ */
+
+// Force LifterLMS into "staging" mode.
+define( 'LLMS_IS_SITE_CLONE', true );
+
+// Force LifterLMS into "live" mode.
+define( 'LLMS_IS_SITE_CLONE', false );
