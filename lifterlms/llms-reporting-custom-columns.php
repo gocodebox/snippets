1fc5f7dad5a42ba20a054925b26715a5d200c54c
Thomas Patrick Levy <thomasplevy@gmail.com>
2022-01-14 14:26:00 -0700

    set export_only to false by default

--- lifterlms/llms-reporting-custom-columns.php
+++ lifterlms/llms-reporting-custom-columns.php
@@ -3,15 +3,17 @@
  * llms-reporting-custom-columns.php
  *
  * @since 2018-01-12
+ * @since 2021-01-14 Switched `export_only` to `false`.
  */
 
 /**
- * Output data for a custom column on the student reporting table/export
- * @param    string     $value    default value being output
- * @param    string     $key      name of the custom field being output
- * @param    obj        $student  LLMS_Student for the row
- * @param    string     $context  output context "display" or "export"
- * @return   mixed
+ * Output data for a custom column on the student reporting table/export.
+ *
+ * @param string       $value   Default value being output.
+ * @param string       $key     Name of the custom field being output.
+ * @param LLMS_Student $student Student object for the row.
+ * @param string       $context Output context "display" or "export".
+ * @return mixed The column value to be output.
  */
 function my_students_table_column_data( $value, $key, $data, $context ) {
 
@@ -26,19 +28,19 @@ add_filter( 'llms_table_get_data_students', 'my_students_table_column_data', 10,
 
 /**
  * Add custom columns to the main students reporting table
- * @param    [type]     $cols  [description]
- * @return   [type]
- * @since    [version]
- * @version  [version]
+ *
+ * @param array[] $cols Array of existing columns.
+ * @return array[]
  */
 function my_students_table_columns( $cols ) {
 
 	$cols['my_custom_field'] = array(
-		'title' => __( 'My Custom Field', 'my-text-domain' ),
-		'export_only' => true, // set to false to show display screens but include in exports
-		'exportable' => true, // set to false to exclude from exports
+		'title'       => __( 'My Custom Field', 'my-text-domain' ),
+		'export_only' => false, // Set to true to hide from screens but still include in exports.
+		'exportable'  => true, // Set to false to exclude from exports.
 	);
 
 	return $cols;
+
 }
 add_filter( 'llms_table_get_students_columns', 'my_students_table_columns', 10 );
