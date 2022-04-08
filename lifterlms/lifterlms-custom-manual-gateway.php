2a18d570f9cf9b9559398a5f52fa2689b52776c3
pondermatic <5377968+pondermatic@users.noreply.github.com>
2022-04-08 08:14:42 -0500

    Fix issue with displaying payment instructions. Update language.

--- lifterlms/lifterlms-custom-manual-gateway.php
+++ lifterlms/lifterlms-custom-manual-gateway.php
@@ -12,11 +12,32 @@ class Custom_Manual_Gateway extends LLMS_Payment_Gateway_Manual {
 
 	public function __construct() {
 		parent::__construct();
-		$this->id                = 'manual_ach';
-		$this->admin_description = __( 'Collect ACH payments manually. Also handles any free orders during checkout.', 'lifterlms' );
-		$this->admin_title       = __( 'Manual ACH', 'lifterlms' );
-		$this->title             = __( 'Manual ACH', 'lifterlms' );
-		$this->description       = __( 'Pay manually via ACH', 'lifterlms' );
+		$this->id                   = 'custom_manual';
+		$this->admin_description    = __( 'Collect payments manually. Also handles any free orders during checkout.', 'lifterlms' );
+		$this->admin_title          = __( 'Custom Manual', 'lifterlms' );
+		$this->title                = __( 'Custom Manual', 'lifterlms' );
+		$this->description          = __( 'Pay manually via custom procedure.', 'lifterlms' );
+		$this->payment_instructions = '';
+	}
+
+	/**
+	 * Output payment instructions if the order is pending.
+	 *
+	 * @since 2022-04-08
+	 */
+	public function before_view_order_table() {
+		global $wp;
+
+		if ( ! empty( $wp->query_vars['orders'] ) ) {
+
+			$order = new LLMS_Order( intval( $wp->query_vars['orders'] ) );
+			if (
+				$order->get( 'payment_gateway' ) === $this->id
+				&& in_array( $order->get( 'status' ), array( 'llms-pending', 'llms-on-hold' ) )
+			) {
+				echo $this->get_payment_instructions();
+			}
+		}
 	}
 }
 
