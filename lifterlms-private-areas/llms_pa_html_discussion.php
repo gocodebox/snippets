9ca2383d9842619b592b2e332fe949d8a05aacde
Kim Coleman <kim@strangerstudios.com>
2023-11-10 07:22:02 -0500

    Updating recipe with proper headers for GitHub embed

--- lifterlms-private-areas/llms_pa_html_discussion.php
+++ lifterlms-private-areas/llms_pa_html_discussion.php
@@ -1,11 +1,12 @@
-<?php // Don't copy this line!
-/**
- * LifterLMS Private Areas filter examples: https://lifterlms.com/docs/private-areas-filters/
- *
- * @since 2017-08-11
- */
-
+<?php
 /**
  * Disable HTML quick tags for discussion (comments) on PA Posts
+ *
+ * Learn more at: https://lifterlms.com/docs/private-areas-filters/
+ * 
+ * You can add this recipe to your site by creating a custom plugin
+ * or using the Code Snippets plugin available for free in the WordPress repository.
+ * Read this companion documentation for step-by-step directions on either method.
+ * https://lifterlms.com/docs/adding-custom-code/
  */
 add_filter( 'llms_pa_html_discussion', '__return_false' );
