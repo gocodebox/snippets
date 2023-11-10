5a923fc63f3f85f7b5958d6c8ac36c5b4fec3d7b
Kim Coleman <kim@strangerstudios.com>
2023-11-10 07:23:37 -0500

    Updating headers to match proper standards

--- lifterlms-private-areas/llms_pa_html_discussion-alt.php
+++ lifterlms-private-areas/llms_pa_html_discussion-alt.php
@@ -1,13 +1,13 @@
-<?php // Don't copy this line!
+<?php
 /**
- * LifterLMS Private Areas filter examples: https://lifterlms.com/docs/private-areas-filters/
+ * Customize the Privaate Areas commenting to allow HTML quicktags for admins only.
  *
- * @since 2017-08-11
- */
-/**
- * customize PA commenting to Allow HTML quicktags for admins only
- * @param    boolean   $bool  default HTML status (true)
- * @return   boolean
+ * Learn more at: https://lifterlms.com/docs/private-areas-filters/
+ * 
+ * You can add this recipe to your site by creating a custom plugin
+ * or using the Code Snippets plugin available for free in the WordPress repository.
+ * Read this companion documentation for step-by-step directions on either method.
+ * https://lifterlms.com/docs/adding-custom-code/
  */
 function my_llms_pa_html_discussion( $bool ) {
 	if ( ! is_admin() ) {
