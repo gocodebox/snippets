9763a5e2e9f656794e84d2dc5a4be8f545d0af2c
actuallyakash <akash_gupta@hotmail.com>
2023-11-15 17:20:23 +0530

    migrated lifterlms-puc-conflicts.php

---
+++ lifterlms/lifterlms-puc-conflicts.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Address incompatibility with the Plugin Update Checker library.
+ * 
+ * https://lifterlms.com/docs/plugin-update-checker-or-puc
+ * 
+ * You can add this recipe to your site by creating a custom plugin
+ * or using the Code Snippets plugin available for free in the WordPress repository.
+ * Read this companion documentation for step-by-step directions on either method.
+ * https://lifterlms.com/docs/adding-custom-code/
+ */
+define( 'LLMS_LOAD_DEPRECATED', false );
