39ae9a9755823b02d49c674817ba73021dfa1b01
Brian Hogg <brian@lifterlms.com>
2025-11-24 12:12:21 -0500

    Update llms-cert-custom-merge-example.php
    Working example for adding custom cert merge codes.

--- lifterlms/llms-cert-custom-merge-example.php
+++ lifterlms/llms-cert-custom-merge-example.php
@@ -2,25 +2,36 @@
 /**
  * llms-cert-custom-merge-example.php
  *
- * @since 2017-10-23
+ * @since 2025-11-24 Updated with more reliable filters.
  */
+
 /**
- * Add custom merge codes to an LLMS certificate
- * @param    int   $user_id          WP_User ID
- * @param    int   $cert_id          WP_Post ID of the generated certificate
- * @param    int   $related_post_id  WP_Post ID of the post which triggered the certificate generation
- * @return   void
+ * Filter the array of available merge codes.
+ * 
+ * @param   array $merge_codes     Array of available merge codes, and their description.
+ *
+ * @return  array
  */
-function my_custom_cert_merge_codes( $user_id, $cert_id, $related_post_id ) {
-		
-	$cert_post = get_post( $cert_id );
+function my_custom_available_certificate_merge_codes( $merge_codes ) {
+	$merge_codes['{your_merge_code}'] = 'Your Merge Code Description';
 
-	$content = str_replace( '{your_merge_code}', get_user_meta( $user_id, 'your_user_meta_key', true ), $cert_post->post_content );
-
-	wp_update_post( array(
-		'ID' => $cert_id,
-		'post_content' => $content
-	) );
-
+	return $merge_codes;
 }
-add_action( 'llms_user_earned_certificate', 'my_custom_cert_merge_codes', 999, 3 );
+add_filter( 'llms_certificate_available_merge_codes', 'my_custom_available_certificate_merge_codes' );
+
+/**
+ * Add custom merge code data to an LLMS certificate.
+ *
+ * @param    array $codes            Array of key (merge code with brackets) and value (data to replace it with)
+ * @param    int   $user_id          WP_User ID
+ * @param    int   $template_id      WP_Post ID of the certificate template
+ * @param    int   $related_post_id  WP_Post ID of the post which triggered the certificate generation
+ *
+ * @return   array
+ */
+function my_custom_certificate_merge_code_data( $codes, $user_id, $template_id, $related_id ) {
+	$codes['{your_merge_code}'] = get_user_meta( $user_id, 'your_user_meta_key', true );
+
+	return $codes;
+}
+add_filter( 'llms_certificate_merge_data', 'my_custom_certificate_merge_code_data', 10, 4 );
